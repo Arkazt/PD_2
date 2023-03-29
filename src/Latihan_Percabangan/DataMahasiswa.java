cd464cc8718e5f4b8eb30e62da88137a53e58dd7
Arkazt <benediktusarion@gmail.com>
2023-03-29 10:43:43 +0700

    menambahkan file percabangan

--- src/Latihan_Percabangan/DataMahasiswa.java
+++ src/Latihan_Percabangan/DataMahasiswa.java
@@ -1,2 +1,51 @@
-package Latihan_Percabangan;public class DataMahasiswa {
+package Latihan_Percabangan;
+
+//dtep 1 import Lib
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+
+public class DataMahasiswa {
+    public static void main(String[] args) {
+        //step 3 deklarasi class buffereadreader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //step 4 deklarasi variabel
+        String nim, nama, prodi;
+        int semester,sksAmbil;
+
+
+        //step 2 error handling
+        //try catch
+        try {
+           //step 5 pengerjaan logika program
+            System.out.println("---- Data Mahasiswa ----"); //header
+            //input
+            System.out.print("NIM : ");
+            nim = br.readLine();
+            System.out.print("Nama : ");
+            nama = br.readLine();
+            System.out.print("Prodi : ");
+            prodi = br.readLine();
+            System.out.print("Semester : ");
+            semester = Integer.parseInt(br.readLine());
+            System.out.print("Jumlah SKS yang diambil : ");
+            sksAmbil = Integer.parseInt(br.readLine());
+
+
+            //output
+            System.out.println("---- Cetak Data Mahasiswa ----");
+            System.out.println("NIM mahasiswa : " + nim);
+            System.out.println("Nama mahasiswa : " + nama);
+            System.out.println("Prodi mahasiswa : " + prodi);
+            System.out.println("Semester mahasiswa : " + semester);
+            System.out.println("Jumlah SKS mahasiswa : " + sksAmbil);
+
+        } catch (Exception exp) {
+            //kondisi ada error, apa yang ditampilkan
+            System.out.println(exp.getMessage());
+            System.out.println("Isian semester dan sks harus angka");
+        }
+    }
 }
--- src/Latihan_Percabangan/percabangan.java
+++ src/Latihan_Percabangan/percabangan.java
@@ -1,2 +1,78 @@
-package Latihan_Percabangan;public class percabangan {
+package Latihan_Percabangan;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class percabangan {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    //if-else
+    //cek syarat pembuatan KTP
+    int usia=0;
+    System.out.print("Masukkan usia anda : ");
+    usia = Integer.parseInt(br.readLine());
+    if (usia >= 17) {
+        System.out.println("Sudah memenuhi syarat");
+    } else {
+        System.out.println("Belum memenuhi syarat");
+    }
+
+    //if-else-if
+    //cek status kelulusan MK berdasarkan nim dan nilai UAS
+    String nim;
+    int nilaiUAS;
+    System.out.println("Masukkan NIM anda : ");
+    nim = br.readLine();
+    System.out.println("Masukkan nilai UAS anda : ");
+    nilaiUAS = Integer.parseInt(br.readLine());
+
+    //jika nilai 80, 70, 65 = lulus, di bawah 65 tidak lulus
+    if (nim.equalsIgnoreCase( "22410100048")
+            && nilaiUAS == 80){
+        System.out.println("lULUS");
+    }else if (nim.equalsIgnoreCase("22410100048")
+            && nilaiUAS == 70){
+        System.out.println("LULUS");
+    }else if (nim.equalsIgnoreCase("22410100048")
+            && nilaiUAS == 65){
+        System.out.println("LULUS");
+    }else if (nim.equalsIgnoreCase("22410100048")
+            && nilaiUAS < 65){
+        System.out.println("TIDAK LULUS");
+    }else {
+        System.out.println("Nilai Tidak Ditemukan");
+    }
+    //equals : memperhatikan detil besar-kecil hurufnya (case sensitive)
+    //equalsIgnoreCase : case non-sensitive
+
+    //nested if
+    //cek menu yang dipilih (makanan/minuman)
+    //makanan = snack, nasi bungkus
+    //minuman = kopi, teh, air mineral
+
+    String menu, jenis;
+    System.out.print("Masukkan menu (Makanan/Minuman) : ");
+    menu = br.readLine();
+    System.out.print("Masukkan jenis menu \n Makanan:Snack, Nasi bungkus \n" + "Minuman: kopi, teh, air mineral. Pilih : ");
+    jenis = br.readLine();
+
+    if (menu.equalsIgnoreCase("makanan")) {
+        if (jenis.equalsIgnoreCase("snack")){
+            System.out.println("harga 2000");
+        }else {
+            System.out.println("harga 5000");
+        }
+    }else if (menu.equalsIgnoreCase("minuman")){
+        if (jenis.equalsIgnoreCase("kopi")){
+            System.out.println("harga 3000");
+        }else if (jenis.equalsIgnoreCase("teh")){
+            System.out.println("harga 2000");
+        }else if (jenis.equalsIgnoreCase("air mineral")){
+            System.out.println("harga 5000");
+        }
+    }
+
+    }
 }
